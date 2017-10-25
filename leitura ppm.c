2c207dccf21cede1c47c38cd9c67f24add530399
victorhugofr <32314062+victorhugofr@users.noreply.github.com>
2017-10-25 08:16:36 -0300

    Update leitura ppm.c

--- leitura ppm.c
+++ leitura ppm.c
@@ -3,30 +3,27 @@
 #include <string.h>
 int main()
 {
-    FILE *ptrimagem;
     char nomeimagem[260];
-    char key[3];
+    char tipo[3];
+    FILE *ptrimagem;
     printf("Digite o nome do arquivo PPM de entrada: ");
-    scanf("%s", nomeimagem);                                // Le o nome do arquivo de entrada
-  
-    ptrimagem=fopen(nomeimagem , "r") ;                         // Abre o arquivo no modo leitura
-    if(ptrimagem == NULL)                                       // Verifica se o arquivo existe e foi aberto
+    scanf("%s", nomeimagem);                               
+    ptrimagem=fopen(nomeimagem , "r") ;                        
+    if(ptrimagem == NULL)                                       
     {
         printf("Erro na abertura do arquivo %s\n", nomeimagem);
         return 0;
     }
-  
-    // Le dados do cabeçalho
-    fscanf(ptrimagem, "%s", key);
-  
+    fscanf(ptrimagem, "%s", tipo);
     // Verifica se a imagem esta em PPM
-    if(strcmp(key,"P3") != 0 && strcmp(key,"P6") != 0)
+    if(strcmp(tipo,"P3") != 0 && strcmp(tipo,"P6") != 0)
     {
         printf("nao eh PPM\n") ;
         fclose(ptrimagem) ;
-        return 0 ;
+        return 0;
     }
-    else{
+    else
+    {
         printf("eh PPM\n");
     }
     return 0;
