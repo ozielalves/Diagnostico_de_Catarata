159ee100c4abd2b04515da239de746ad4bd669d6
victorhugofr <32314062+victorhugofr@users.noreply.github.com>
2017-11-01 07:27:26 -0300

    Update conversaocinza.c

--- conversaocinza.c
+++ conversaocinza.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-  
-// Recebe inteiros M e N que representam a largura
-// e a altura da imagem e aloca memoria para uma
-// matriz de dimencoes (N x M)
 unsigned char** aloca(int m, int n)
 {
     int i;
@@ -16,9 +12,7 @@ unsigned char** aloca(int m, int n)
     }
     return M;
 }
-  
-// Metodo da combinacao linear
-int combLinear(unsigned char r, unsigned char g, unsigned char b)
+int conversaocinza(unsigned char r, unsigned char g, unsigned char b)
 {
     int new;
     new = 0.3*r + 0.59*g + 0.11*b;
@@ -26,81 +20,69 @@ int combLinear(unsigned char r, unsigned char g, unsigned char b)
 } 
 int main()
 {
-    FILE *image; // Ponteiro para o arquivo que contem a imagem que vamos tratar
-    FILE *newImage; // Ponteiro para o arquivo onde vamos salvar a imagem tratada
-    char image_name[256];
-    char newImage_name[256];
+    FILE *ptrimagem; 
+    FILE *ptrnovaimagem;
+    char nomeimagem[260];
+    char nomenovaimagem[260];
     char key[2];
     unsigned char red, green, blue;
     int i, j, m, n, max;
   
     printf("Digite o nome do arquivo PPM de entrada: ");
-    scanf("%s", image_name); // Le o nome do arquivo de entrada
+    scanf("%s", nomeimagem);
     printf("Digite o nome do arquivo PGM de saida: ");
-    scanf("%s", newImage_name);
+    scanf("%s", nomenovaimagem);
   
-    image = fopen(image_name , "r") ; // Abre o arquivo no modo leitura
-    if(image == NULL) // Verificase o arquivo existe e foi aberto
+    ptrimagem = fopen(nomeimagem , "r") ;
+    if(ptrimagem == NULL) // Verificase o arquivo existe e foi aberto
     {
-        printf("Erro na abertura do arquivo %s\n", image_name);
+        printf("Erro na abertura do arquivo %s\n", nomeimagem);
         return 0 ;
     }
-  
-    // Le dados do cabeçalho
-    fscanf(image, "%s", key);
-  
-    // Verifica se a imagem esta em PPM
+    fscanf(ptrimagem, "%s", key);
     if(strcmp(key,"P3") != 0)
     {
         printf("Arquivo nao e um PPM\n") ;
-        fclose(image) ;
+        fclose(ptrimagem) ;
         return 0 ;
     }
-  
-    // Le os outros dados do cabecalho
-    fscanf(image, "%d %d %d", &m, &n, &max) ;
-    // m guarda a largura da imagem (colunas da matriz)
-    // n guarda a altura da imagem (linhas da matriz)
-    // max guarda o valor máximo da escala
-  
-    // Matriz para guardar a imagem em tons de cinza
-    unsigned char **grayImage;
-    grayImage = aloca(m, n);
-  
-    // Lê os canais de cores de cada pixel ij da imagem
-    // e salva na matriz grayImage o seu correspondente
-    // em cinza
+    fscanf(ptrimagem, "%d %d %d", &m, &n, &max) ;
+    /* m guarda a largura da imagem (colunas da matriz)
+     n guarda a altura da imagem (linhas da matriz)
+    max guarda o valor máximo da escala
+    Matriz para guardar a imagem em tons de cinza*/
+    unsigned char **matrizcinza;
+    matrizcinza = aloca(m, n);
+    /* Lê os canais de cores de cada pixel ij da imagem
+     e salva na matriz matrizcinza o seu correspondente
+     em cinza*/
     for(i = 0; i < n; i++)
     {
         for(j = 0; j < m; j++)
         {
-            fscanf(image, "%c%c%c", &red, &green, &blue);
-            grayImage[i][j] = combLinear(red, green, blue);
-            //grayImage[i][j] = media(red, green, blue);
+            fscanf(ptrimagem, "%c%c%c", &red, &green, &blue);
+            matrizcinza[i][j] = conversaocinza(red, green, blue);
         }
     }
   
-    newImage = fopen(newImage_name , "w"); // Abre o arquivo no modo escrita
-    fprintf(newImage,"P5\n"); // Escreve o cabecalho arquivo PGM
-    fprintf(newImage, "%d %d\n %d\n", m, n, max); // Escreve o cabecalho
+    ptrnovaimagem = fopen(nomenovaimagem , "w"); 
+    fprintf(ptrnovaimagem,"P5\n"); 
+    fprintf(ptrnovaimagem, "%d %d\n %d\n", m, n, max); 
   
     for(i = 0; i < n; i++)
     {
         for(j = 0; j < m; j++)
         {
             // Escreve os valores de cada pixel no arquivo
-            fprintf(newImage, "%c", (char) grayImage[i][j]);
+            fprintf(ptrnovaimagem, "%c", (char) matrizcinza[i][j]);
         }
     }
-  
-    fclose(image); // Fecha o arquivo da imagem original
-    fclose(newImage); // Fecha o arquivo da nova imagem
-  
-    // Libera a memória alocada para a matriz grayImage
+    fclose(ptrimagem);
+    fclose(ptrnovaimagem);
     for(i = 0; i < n; i++)
     {
-        free(grayImage[i]);
+        free(matrizcinza[i]);
     }
-    free(grayImage);
+    free(matrizcinza);
     return 0;
 }
