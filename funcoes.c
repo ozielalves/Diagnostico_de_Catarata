138918bf1a65cc7e5ee58e8bd257ad79e2a94ebc
victorhugofr <32314062+victorhugofr@users.noreply.github.com>
2017-11-29 13:26:25 -0200

    Maior parte dos comentários feitos(falta terminar)

--- funcoes.c
+++ funcoes.c
@@ -1,6 +1,6 @@
 #include "funcoes.h"
 #include "structs.h"
-void criarmatriz(pont_imagem Imagem){ /*Prepara os pixels da nova imagem recebendo a altura e largura da imagem original*/
+void criarmatriz(pont_imagem Imagem){ /*Prepara os pixels da nova imagem recebendo a altura e largura da imagem*/
     int i;
     Imagem->pixelimagem = (pixel**)malloc(Imagem->altura * sizeof(pixel*));
     for(i=0; i<Imagem->altura; i++){
@@ -19,9 +19,9 @@ void limparimagem(pont_imagem Imagem) {
 centro *hough(pont_imagem Imagem){
     unsigned short int t,r,i,j,x,y,k,l;
     unsigned short int altura = Imagem->altura, largura = Imagem->largura;
-    unsigned short int raiominimoiris = (fmin(altura,largura))/10; //raio da iris é em torno do raio da pupila x2
-    unsigned short int raiomaximopupila = (fmin(altura,largura))/4; // pode ser que a altura  ou largura seja o maior, entao o maximo sera em torno do maior/4
-    unsigned short int raiominimopupila = raiomaximopupila/5; // minimo em torno do raio maximo /4
+    unsigned short int raiominimoiris = (fmin(altura,largura))/8; //raio da iris é em torno do raio da pupila x2
+    unsigned short int raiomaximopupila = (fmin(altura,largura))/4; // pode ser que a altura  ou largura seja o maior, entao o maximo sera em torno do menor/4
+    unsigned short int raiominimopupila = raiomaximopupila/4; // minimo em torno do raio maximo /4
     unsigned short int r_max,countraios=0,raios[raiomaximopupila-raiominimopupila+1];
     int centrofinali, centrofinalj;
     int dim,centroi=0, centroj=0,aux=0;
@@ -33,7 +33,7 @@ centro *hough(pont_imagem Imagem){
                 for(k = raiominimopupila; k <= raiomaximopupila; k++){ // k= area dos pixels da pupila
                     dim = (k-raiominimopupila)*largura*altura; // convesao de espaço tridimensional para um vetor unidimensional(por causa da formula)
                     for(t = 0; t < 360; t++){
-                        y = i + k * sin(t*PI/180.0); // formula do pdf errada? com o i- k*senos[t] ele não gira corretamente. coloquei + para dar uma volta no sentido antihorario
+                        y = i + k * sin(t*PI/180.0); 
                         x = j - k * cos(t*PI/180.0); // formula dada, achando coordenadas (x,y) 
                         A[dim+(y*largura)+x]++; // espaço de hugh
                     }
@@ -51,7 +51,7 @@ centro *hough(pont_imagem Imagem){
     for(i = raiominimopupila; i < altura-raiomaximopupila; i++){
         for(j = raiominimopupila; j < largura-raiomaximopupila; j++){
             for(k = raiominimoiris; k <= raiomaximopupila; k++){
-                if(A[largura*altura*(k-raiominimopupila)+(i*largura)+j] > 0.875*max){
+                if(A[largura*altura*(k-raiominimopupila)+(i*largura)+j] > 0.9*max){
                 aux++;
                 centroi+=i;
                 centroj+=j;
@@ -62,6 +62,9 @@ centro *hough(pont_imagem Imagem){
     centrofinali = centroi*1.0/aux; // enfim, calculamos a media
     centrofinalj = centroj*1.0/aux;
     centro *c = malloc(sizeof(centro));
+    if(c == NULL){
+    	fprintf(stderr, "Erro na alocacao do centro da pupila");
+    }
     c->y = centrofinali; // coordenada y do centro
     c->x = centrofinalj; // coordenada x do centro
     c->r = 0;
@@ -77,7 +80,7 @@ centro *hough(pont_imagem Imagem){
     for(l = 0; l < countraios; l++){
         if(l == countraios-1){
             c->r = raios[l]; // nesse caso so há um raio = raios[0]
-        }else if(raios[l] > (raios[l+1])/3.0){// caso d do pdf, caso a imagem contenha mais de um circulo,  
+        }else if(raios[l] > (raios[l+1])/3.0){// caso d do pdf, caso a imagem contenha mais de um circulo.
             c->r = raios[l];
             if(l+1 == countraios-1)
             break;
@@ -93,7 +96,7 @@ pont_imagem segmentacao(pont_imagem Imagem, centro *c){
     Imagemsegmentada->max = Imagem->max;
     criarmatriz(Imagemsegmentada);
     int i,j,raioimagem;
-    if(Imagemsegmentada->largura == 1167){
+    if(Imagemsegmentada->largura == 1167){/*Cada imagem possui dimensoes diferentes, logo, raios diferentes*/
         raioimagem=20;
     }
     else if (Imagemsegmentada->largura==1015){
@@ -107,11 +110,14 @@ pont_imagem segmentacao(pont_imagem Imagem, centro *c){
    }
     for(i = 0; i < Imagem->altura; i++){
         for(j = 0; j < Imagem->largura; j++){
-        if(sqrt((i-c->y)*(i-c->y)+(j-c->x)*(j-c->x)) < c->r+raioimagem){ // ajuste manual do raio... ???
+        if(sqrt((i-c->y)*(i-c->y)+(j-c->x)*(j-c->x)) < c->r+raioimagem){ 
+		/*Nesse caso, foi necessário identificar manualmente o valor especifico do raio da imagem.  Caso o círculo seja excêntrico à pupila,
+		todos os valores menor que o raio deste circulo, sera pertencente a pupila.*/
             Imagemsegmentada->pixelimagem[i][j].r = Imagem->pixelimagem[i][j].r;
             Imagemsegmentada->pixelimagem[i][j].g = Imagem->pixelimagem[i][j].g;
             Imagemsegmentada->pixelimagem[i][j].b = Imagem->pixelimagem[i][j].b;
         }else{
+		/*Caso não, será preto.(Não pertencente a imagem.)*/
             Imagemsegmentada->pixelimagem[i][j].r = 0;
             Imagemsegmentada->pixelimagem[i][j].g = 0;
             Imagemsegmentada->pixelimagem[i][j].b = 0;
@@ -125,7 +131,7 @@ pont_imagem filtrosobel(pont_imagem Imagem){
     int x=0, y=0,i, j, k, l;
     int teste;
     short filtro;
-    const char sobelx[3][3] = {{ 1, 0, -1},{ 2, 0, -2},{1,0,-1}};
+    const char sobelx[3][3] = {{ 1, 0, -1},{ 2, 0, -2},{1,0,-1}};//filtros
     const char sobely[3][3] = {{1, 2, 1},{0, 0, 0},{-1, -2, -1}};
     pont_imagem Imagemsobel = calloc(1,sizeof(imagem));
     strcpy(Imagemsobel->codigo,Imagem->codigo);
@@ -133,20 +139,20 @@ pont_imagem filtrosobel(pont_imagem Imagem){
     Imagemsobel->largura = Imagem->largura;
     Imagemsobel->max = Imagem->max;
     criarmatriz(Imagemsobel);
-    for(i=1; i<(Imagem->altura)-1; i++){                /*Aqui deve-se retirar as bordas da imagem para nao existir indices negativos*/
+/*Aqui deve-se retirar as bordas da imagem para nao existir indices negativos*/
+    for(i=1; i<(Imagem->altura)-1; i++){                
         for(j=1; j< (Imagem->largura)-1; j++){
             x=0;
             y=0;
             for(k=0; k<3; k++){
                 for(l=0; l<3; l++){ 
-                    x += ((Imagem->pixelimagem[i-1+k][j-1+l].r) * (sobelx[k][l]));
+                    x += ((Imagem->pixelimagem[i-1+k][j-1+l].r) * (sobelx[k][l]));// definição do Gx e Gy
                     y += ((Imagem->pixelimagem[i-1+k][j-1+l].r) * (sobely[k][l]));
 
                 }
             }
-            int aux = sqrt(pow(x,2)+pow(y,2));
-            //unsigned char aux=255-(unsigned char)(filtro);
-                        if(aux > 255){
+            int aux = sqrt(pow(x,2)+pow(y,2)); //fórmula do filtro de sobel.
+                        if(aux > 255){ //limite maximo da imagem.
                             aux=255;
                         }
                 Imagemsobel->pixelimagem[i][j].r = aux;
@@ -160,6 +166,10 @@ pont_imagem filtrosobel(pont_imagem Imagem){
 pont_imagem binarizacao(pont_imagem Imagem){
     int  i,j,threshold;
     pont_imagem Imagembin = calloc(1,sizeof(imagem));
+    if(Imagembin==NULL){
+	fprintf(stderr, "Erro ao alocar memoria para imagem binarizada.");
+	return NULL;
+    }
     strcpy(Imagembin->codigo,Imagem->codigo);
     Imagembin->altura = Imagem->altura;
     Imagembin->largura = Imagem->largura;
@@ -180,6 +190,8 @@ pont_imagem binarizacao(pont_imagem Imagem){
     for(i = 0; i < Imagem->altura; i++){
         for(j = 0; j < Imagem->largura; j++){
             if(Imagem->pixelimagem[i][j].r > threshold){
+		    /*Caso a intensidade do pixel seja maior que o threshold, tornar ele branco, senão, torne-o preto.
+		    Importante observar que cada imagem possui um threshold diferente*/
                 Imagembin->pixelimagem[i][j].r = 255;               
                 Imagembin->pixelimagem[i][j].g = 255;
                 Imagembin->pixelimagem[i][j].b = 255;
@@ -199,6 +211,10 @@ pont_imagem filtrogaussiano(pont_imagem Imagem){
     int aux;
     int filtro[5][5] = {{ 2,  4,  5,  4, 2 },{ 4,  9, 12,  9, 4 },{ 5, 12, 15, 12, 5 },{ 4,  9, 12,  9, 4 },{ 2,  4,  5,  4, 2 }};
     pont_imagem Imagemgauss = calloc(1,sizeof(imagem));
+	if(Imagemgauss==NULL){
+	fprintf(stderr, "Erro ao alocar memoria para imagem com filtro gaussiano.");
+	return NULL;
+    }
     strcpy(Imagemgauss->codigo,Imagem->codigo);
     Imagemgauss->altura = Imagem->altura;
     Imagemgauss->largura = Imagem->largura;
@@ -228,7 +244,14 @@ pont_imagem lerimagem(FILE *imagem){/*Lê a imagem do usuário*/
     int temp1,temp2;
     pont_imagem Imagem;
     Imagem=malloc(sizeof(imagem));
-    while(1){
+    if(Imagem==NULL){
+	fprintf(stderr, "Erro ao alocar memoria para a imagem.");
+	return NULL;
+    }
+	/*Logo abaixo, é verificado se existe uma linha de comentário, e assim será feito no decorrer da leitura:
+	O programa lerá infinitamente até que o primeiro char seja diferente de "#", e quando parado, o valor ou palavra sera usado nas
+	variáveis requisitadas.(altura,largura,max,pixeis).*/
+    while(1){			
         fscanf(imagem,"%s",temp);
         if(temp[0]=='#'){
             fgets(aux,99,imagem);
@@ -236,7 +259,7 @@ pont_imagem lerimagem(FILE *imagem){/*Lê a imagem do usuário*/
             break;
         }
     }
-    if(strcmp(temp,"P3")!=0){ // Verifica se é PPM(incompleto)
+    if(strcmp(temp,"P3")!=0){ // Verifica se é PPM
         printf("Imagem nao eh PPM\n");
         exit(1);
     }
@@ -250,6 +273,7 @@ pont_imagem lerimagem(FILE *imagem){/*Lê a imagem do usuário*/
             break;
         }
     }
+	/*No caso da altura e largura, devemos utilizar sscanf, pois nas imagens, elas se encontram na mesma linha.*/
     sscanf(temp,"%d %d",&temp1,&temp2);
     Imagem->largura=(temp1);
     Imagem->altura=(temp2);
@@ -287,13 +311,15 @@ void novaimagem(pont_imagem Imagem, unsigned short int a){/*Função para criar 
 	}
     scanf("%s", novonome);
     imagem=fopen(novonome,"w");// abre em modo escrita
-    fprintf(imagem,"%s\n",Imagem->codigo);/*escreve o cabeçalho da imagem*/
+    fprintf(imagem,"%s\n",Imagem->codigo);/*escreve o cabeçalho da imagem.*/
     fprintf(imagem,"%i ",Imagem->largura);
     fprintf(imagem,"%i\n",Imagem->altura);
     fprintf(imagem,"%i\n",Imagem->max);
     for(i=0; i<Imagem->altura; i++){
         for(j=0; j<Imagem->largura; j++){
-            fprintf(imagem,"%i ",Imagem->pixelimagem[i][j].r);/*Escreve os novos pixels, ja alterados na conversão*/
+            fprintf(imagem,"%i ",Imagem->pixelimagem[i][j].r);/*Escreve os novos pixels, em formato mais didático: p1 p2 p3
+	    													   p4 p5 p6
+														   p7 p8 p9*/
             fprintf(imagem,"%i ",Imagem->pixelimagem[i][j].g);
             fprintf(imagem,"%i\n",Imagem->pixelimagem[i][j].b);
         }
@@ -304,6 +330,10 @@ void novaimagem(pont_imagem Imagem, unsigned short int a){/*Função para criar 
 pont_imagem transformarcinza(pont_imagem Imagem){
     int i, j,combinacao;
     pont_imagem Imagemcinza = calloc(1,sizeof(imagem));
+    if(Imagemcinza==NULL){
+	fprintf(stderr, "Erro ao alocar memoria para imagem em tons de cinza.");
+	return NULL;
+    }
     strcpy(Imagemcinza->codigo,Imagem->codigo);
     Imagemcinza->altura = Imagem->altura;
     Imagemcinza->largura = Imagem->largura;
@@ -333,7 +363,7 @@ double porcentagem(pont_imagem Imagem) {
         }
     }
 
-    porcentagemfinal = 100*(qtdpixelafetado/qtdpixel);
+    porcentagemfinal = 100*(qtdpixelafetado/qtdpixel);//retornando a porcentagem em um numero inteiro
     return porcentagemfinal;
 }
 void ndiagnostico(double porcentagemfinal) {
@@ -342,7 +372,7 @@ void ndiagnostico(double porcentagemfinal) {
     if (arquivo == NULL) {
         fprintf(stderr, "Erro na criacao do arquivo diagnostico\n");
         exit(1);
-    }
+    } /*Caso a pupila tenha 65% ou mais de pixels comprometidos(valor intermediário entre 60% e 70% dado no pdf), o indivíduo será diagnosticado com catarata*/
     if (porcentagemfinal >= 65) {
         fprintf(arquivo, "Indivíduo com catarata\nPorcentagem de comprometimento da pupila: %.2lf%%\n",porcentagemfinal);
     }
@@ -383,6 +413,10 @@ void marcarpupila(pont_imagem Imagem, centro *c) {
 }
 pont_imagem flash(pont_imagem Imagem){
     pont_imagem Imagemsflash = calloc(1,sizeof(imagem));
+    if(Imagemsflash==NULL){
+	fprintf(stderr, "Erro ao alocar memoria para imagem binarizada.");
+	return NULL;
+    }
     strcpy(Imagemsflash->codigo,Imagem->codigo);
     Imagemsflash->altura = Imagem->altura;
     Imagemsflash->largura = Imagem->largura;
@@ -391,7 +425,7 @@ pont_imagem flash(pont_imagem Imagem){
     unsigned short int i,j,media;
     int soma,total;
     for(i = 0; i < Imagem->altura; i++){
-        for(j = 0; j < Imagem->largura; j++){
+        for(j = 0; j < Imagem->largura; j++){ /*Aqui realizamos a média dos tons no centro da imagem(uma tentativa de deixar a imagem uniforme).*/
             if(Imagem->pixelimagem[i][j].r > 0){
                 soma+=Imagem->pixelimagem[i][j].r;
                 total++;
@@ -401,11 +435,11 @@ pont_imagem flash(pont_imagem Imagem){
     media=(int)soma/total;
     for(i = 0; i < Imagem->altura; i++){
         for(j = 0; j < Imagem->largura; j++){
-            if(Imagem->pixelimagem[i][j].r < 140){
+            if(Imagem->pixelimagem[i][j].r < 140){/*Caso o pixel seja menor que 140(valor manual achado para tonalidade de um flash), será um valor real da pupila.*/
                 Imagemsflash->pixelimagem[i][j].r = Imagem->pixelimagem[i][j].r;
                 Imagemsflash->pixelimagem[i][j].g = Imagem->pixelimagem[i][j].g;
                 Imagemsflash->pixelimagem[i][j].b = Imagem->pixelimagem[i][j].b;
-            }else{
+            }else{/*Se não, substitua-o pela média das tonalidades.*/
                 Imagemsflash->pixelimagem[i][j].r = media;
                 Imagemsflash->pixelimagem[i][j].g = media;
                 Imagemsflash->pixelimagem[i][j].b = media;
